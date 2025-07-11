9c1cd9f8a21df0bfdeeafe506b11286dab8ece92
Aimen iqbal <157978710+aimenasthatic@users.noreply.github.com>
2025-07-11 17:30:17 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -23,3 +23,27 @@ function App() {
 }
 
 export default App;
+=======
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
+import Timer from "./three";
+
+function App() {
+  const [isVisible, setVisible] = useState(true);
+  useEffect(() => {
+    console.log("App comp. is mounting....");
+  }, []);
+
+  return (
+    <>
+      <div className="toggle">
+        {isVisible ? <Timer /> : null}
+        <button onClick={() => setVisible(!isVisible)}> Toggle</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
